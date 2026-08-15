14b14c117fd5e9731e6765303df70783129fbc56
Fares556259 <unleashedfares@gmail.com>
2026-08-15 05:08:55 +0100

    Add Level 2 configuration

--- src/lib/report-cards/level-config.ts
+++ src/lib/report-cards/level-config.ts
@@ -42,5 +42,35 @@ export const LEVEL_CONFIGS: Record<number, LevelConfig> = {
         ],
       }
     ]
+  },
+  2: {
+    domains: [
+      {
+        name: "مجال العربية",
+        subjects: [
+          { search: "التواصل الشفوي", display: "تواصل شفوي" },
+          { search: "الخط", display: "الخط" },
+          { search: "القراءة", display: "القراءة" },
+          { search: "الإنتاج الكتابي", display: "الانتاج الكتابي" },
+        ],
+      },
+      {
+        name: "مجال العلوم",
+        subjects: [
+          { search: "الرياضيات", display: "الرياضيات" },
+          { search: "الإيقاظ العلمي", display: "الايقاظ العلمي" },
+          { search: "التربية التكنولوجية", display: "التربية التكنولوجية" },
+        ],
+      },
+      {
+        name: "مجال التنشئة",
+        subjects: [
+          { search: "التربية الإسلامية", display: "تربية اسلامية" },
+          { search: "التربية الموسيقية", display: "تربية موسيقية" },
+          { search: "التربية التشكيلية", display: "تربية تشكيلية" },
+          { search: "التربية البدنية", display: "تربية بدنية" },
+        ],
+      }
+    ]
   }
 };
